d3f622c465ee70e5242a298fcaec3c9540d726de
luisbaiao <luis.baiao@dpaschoal.com.br>
2023-08-24 09:12:55 -0300

    Implementado cadastro de Produtos

--- src/main/java/br/com/baiao/api/services/ProductService.java
+++ src/main/java/br/com/baiao/api/services/ProductService.java
@@ -1,10 +1,13 @@
 package br.com.baiao.api.services;
 
+import br.com.baiao.api.model.Category;
 import br.com.baiao.api.model.Product;
+import br.com.baiao.api.repository.CategoryRepository;
 import br.com.baiao.api.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,6 +16,8 @@ public class ProductService {
     @Autowired
     ProductRepository repositorio;
 
+    @Autowired
+    CategoryRepository categoryRepository;
     public List<Product> retornarTodosOsProdutos(){
         return repositorio.findAll();
     }
@@ -22,6 +27,13 @@ public class ProductService {
     }
 
     public void salvaProduto(Product produto){
+        if (produto.getCategories() !=null){
+            List<Category> categoriasAdicionar = new ArrayList<>();
+            for(Category category : produto.getCategories()){
+                categoriasAdicionar.add(categoryRepository.getReferenceById(category.getId()));
+            }
+            produto.setCategories(categoriasAdicionar);
+        }
         repositorio.save(produto);
     }
 
